1b0583daaf08f73ae96048e2189b8828f0b529f1
Nivesh Syreddy <nivesh.syreddy@zemosolabs.com>
2022-04-29 16:26:53 +0530

    Added Loops file

---
+++ src/com/corejava/zemoso/Loops.java
@@ -0,0 +1,25 @@
+package com.corejava.zemoso;
+
+public class Loops {
+    public static void main(String[] args) {
+        int count = 0;
+
+        // While loop
+//        while (count <= 10){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // do while loop
+//        do {
+//            System.out.println(count);
+//            count ++;
+//        }
+//        while (count <= 10);
+
+        // for loop
+        for (int iter=0;iter<=10;iter++){
+            System.out.println(iter);
+        }
+    }
+}
