216dad1150d496fd737fb21b738263b4e59682c3
Nivesh Syreddy <nivesh.syreddy@zemosolabs.com>
2022-04-29 10:52:15 +0530

    Variables file added

---
+++ src/com/corejava/zemoso/Variables.java
@@ -0,0 +1,11 @@
+package com.corejava.zemoso;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Nivesh";
+        final String HOME_TOWN = "Hyderabad";
+        System.out.println(name+" is "+age);
+        System.out.println(name+" Home Town is "+HOME_TOWN);
+    }
+}
