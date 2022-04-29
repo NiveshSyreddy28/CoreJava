820e10d333da5b5dc70858a54b93613ba290da09
Nivesh Syreddy <nivesh.syreddy@zemosolabs.com>
2022-04-29 11:18:41 +0530

    Comments file added

---
+++ src/com/corejava/zemoso/Comments.java
@@ -0,0 +1,10 @@
+package com.corejava.zemoso;
+
+public class Comments {
+    public static void main(String[] args) {
+        //I'm single line comment
+        /*We are
+        multi line comments
+         */
+    }
+}
